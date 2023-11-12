ae20d5ab17f4a2ebc8bc8bb739fe7e8d1bf56311
Adam B <11babiad11@gmail.com>
2023-11-12 17:17:38 +0100

    Uprava metody FloodFill() a přidání vlajek ("F") //Občas dost zabugovane

--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -1,4 +1,6 @@
-﻿using static System.Reflection.Metadata.BlobBuilder;
+﻿using Microsoft.VisualBasic.Devices;
+using System.Runtime.CompilerServices;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Minesweeper
 {
@@ -6,6 +8,8 @@ namespace Minesweeper
     {
         private int[,] grid;
         private int bombs;
+
+        private bool gameActive = true;
 
         private Button resetButton;
 
@@ -28,8 +32,8 @@ namespace Minesweeper
         private void InitializeGame()
         {
             //            [x, y]
-            grid = new int[5, 5];
-            bombs = 10;
+            grid = new int[9, 9];
+            bombs = 7;
 
             GenerateBombs(grid, bombs);
             AroundBombs(grid);
@@ -98,9 +102,8 @@ namespace Minesweeper
             }
         }
 
-        public void ShowGrid(int[,] grid)
+        public void DeleteGridButtons()
         {
-            // Odstranění existujících tlačítek hrací plochy
             foreach (Control control in Controls.OfType<Button>().ToList())
             {
                 if (control != resetButton)
@@ -109,6 +112,11 @@ namespace Minesweeper
                     control.Dispose();
                 }
             }
+        }
+
+        public void ShowGrid(int[,] grid)
+        {
+            DeleteGridButtons();
 
             for (int x = 0; x < grid.GetLength(0); x++)
             {
@@ -120,6 +128,7 @@ namespace Minesweeper
                     b.Top = y * 40 + 40;
                     b.Height = 40;
                     b.Text = $"[{x},{y}]";
+                    b.MouseDown += ToggleFlag;
                     Controls.Add(b);
 
                     //Convertuje zkryte prvky v gridu
@@ -155,7 +164,6 @@ namespace Minesweeper
                         case 8:
                             b.Click += Click8;
                             break;
-
                     }
                 }
             }
@@ -164,13 +172,172 @@ namespace Minesweeper
         private void ResetGame()
         {
             InitializeGame();
-
             ShowGrid(grid);
+            gameActive = true;
         }
 
+        private Button GetButtonAt(int x, int y)
+        {
+            int targetLeft = x * 40;
+            int targetTop = y * 40 + 40;
 
+            foreach (Control control in Controls)
+            {
+                if (control is Button button && button.Left == targetLeft && button.Top == targetTop)
+                {
+                    return button;
+                }
+            }
+            return null;
+        }
 
-        
+        private void RevealAllCells()
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    Button b = GetButtonAt(x, y);
+                    b.Enabled = false; // Disable further clicks
+
+                    switch (grid[x, y])
+                    {
+                        case 9:
+                            b.Text = "X";
+
+                            break;
+                        case 0:
+                            b.Text = " ";
+                            break;
+                        case 1:
+                            b.Text = "1";
+                            break;
+                        case 2:
+                            b.Text = "2";
+                            break;
+                        case 3:
+                            b.Text = "3";
+                            break;
+                        case 4:
+                            b.Text = "4";
+                            break;
+                        case 5:
+                            b.Text = "5";
+                            break;
+                        case 6:
+                            b.Text = "6";
+                            break;
+                        case 7:
+                            b.Text = "7";
+                            break;
+                        case 8:
+                            b.Text = "8";
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void FloodFill(int x, int y)
+        {
+            // Check if the coordinates are within the grid bounds
+            if (x < 0 || x >= grid.GetLength(0) || y < 0 || y >= grid.GetLength(1))
+            {
+                return;
+            }
+
+            Button currentButton = GetButtonAt(x, y);
+
+            // Check if the current cell is already revealed or has a bomb
+            if (currentButton.Text == " " || currentButton.Text == "F" || grid[x, y] == 9)
+            {
+                return;
+            }
+
+            // Count the number of adjacent bombs
+            int adjacentBombs = CountAdjacentBombs(x, y);
+            int flaggedCount = CountFlaggedCells(x, y);
+
+            // Reveal the current cell with the appropriate number or an empty space
+            currentButton.Text = (adjacentBombs > 0) ? adjacentBombs.ToString() : " ";
+
+            // If the current cell has no adjacent bombs, recursively call flood-fill on its neighbors
+            if (adjacentBombs == flaggedCount)
+            {
+                for (int i = x - 1; i <= x + 1; i++)
+                {
+                    for (int j = y - 1; j <= y + 1; j++)
+                    {
+                        if (i != x || j != y) // Skip the current cell
+                        {
+                            FloodFill(i, j);
+                        }
+                    }
+                }
+            }
+        }
+
+        private int CountAdjacentBombs(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (i >= 0 && i < grid.GetLength(0) && j >= 0 && j < grid.GetLength(1) && grid[i, j] == 9)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private int CountFlaggedCells(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (i >= 0 && i < grid.GetLength(0) && j >= 0 && j < grid.GetLength(1))
+                    {
+                        Button button = GetButtonAt(i, j);
+                        if (button != null && button.Text == "F")
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+        private void ToggleFlag(object sender, MouseEventArgs e)
+        {
+            if (gameActive)
+            {
+                Button button = (Button)sender;
+                int x = button.Left / 40;
+                int y = (button.Top - 40) / 40;
+
+                if (e.Button == MouseButtons.Right)
+                {
+                    if (button.Text == $"[{x},{y}]")
+                    {
+                        button.Text = "F"; // Umístění vlajky
+                    }
+                    else if (button.Text == "F")
+                    {
+                        button.Text = $"[{x},{y}]"; // Odstranění vlajky
+                    }
+                }
+            }
+        }
+
 
         private void Reset_Click(object sender, EventArgs e)
         {
@@ -180,65 +347,103 @@ namespace Minesweeper
         //Info na gridu po kliknuti
         private void Click9(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            b.Text = "X";
+            if (gameActive)
+            {
+                Button b = (Button)sender;
+                b.Text = "X";
+                RevealAllCells();
+                b.BackColor = Color.Red;
+                gameActive = false;
+            }
         }
         private void Click0(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            b.Text = " ";
+            if (gameActive)
+            {
+                Button b = (Button)sender;
+                int x = b.Left / 40;
+                int y = (b.Top - 40) / 40;
+
+                if (b.Text == " ")
+                    return;
+
+                FloodFill(x, y);
+            }
         }
         private void Click1(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            b.Text = "1";
-            //b.BackColor = Color.LightBlue;
+            if (gameActive)
+            {
+                Button b = (Button)sender;
+                b.Text = "1";
+                //b.BackColor = Color.LightBlue;
+            }
         }
         private void Click2(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            b.Text = "2";
-            //b.BackColor = Color.LightGreen;
-
+            if (gameActive)
+            {
+                Button b = (Button)sender;
+                b.Text = "2";
+                //b.BackColor = Color.LightGreen;
+            }
         }
         private void Click3(object sender, EventArgs e)
         {
-            Button b = (Button)sender;
-            b.Text = "3";
-            //b.BackColor = Color.IndianRed;
+            if (gameActive)
+            {
+                Button b = (Button)sender;
+                b.Text = "3";
+                //b.BackColor = Color.IndianRed;
+            }
 
         }
         private void Click4(object sender, EventArgs e)
         {
-            //tmave modra
-            Button b = (Button)sender;
-            b.Text = "4";
-            //b.BackColor = Color.DarkBlue;
+            if (gameActive)
+            {
+                //tmave modra
+                Button b = (Button)sender;
+                b.Text = "4";
+                //b.BackColor = Color.DarkBlue;
+            }
 
         }
         private void Click5(object sender, EventArgs e)
         {
-            //tmave cervena
-            Button b = (Button)sender;
-            b.Text = "5";
+            if (gameActive)
+            {
+                //tmave cervena
+                Button b = (Button)sender;
+                b.Text = "5";
+            }
         }
         private void Click6(object sender, EventArgs e)
         {
-            //sedo modra idk
-            Button b = (Button)sender;
-            b.Text = "6";
+            if (gameActive)
+            {
+                //sedo modra idk
+                Button b = (Button)sender;
+                b.Text = "6";
+            }
         }
         private void Click7(object sender, EventArgs e)
         {
-            //cerna
-            Button b = (Button)sender;
-            b.Text = "7";
+            if (gameActive)
+            {
+                //cerna
+                Button b = (Button)sender;
+                b.Text = "7";
+            }
         }
         private void Click8(object sender, EventArgs e)
         {
-            //seda
-            Button b = (Button)sender;
-            b.Text = "8";
+            if (gameActive)
+            {
+                //seda
+                Button b = (Button)sender;
+                b.Text = "8";
+            }
         }
     
     }
